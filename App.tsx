d586d884caa804c1a3c0034e7c0e7c5d461c2e9d
Simone Taeggi <startae14@gmail.com>
2024-09-17 11:15:27 +0200

    define common theme and Style in React Native

--- App.tsx
+++ App.tsx
@@ -1,24 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          borderBottomWidth: 1,
-          borderBottomColor: "#1a759f",
-          paddingHorizontal: 8,
-          paddingVertical: 16,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 18,
-            fontWeight: "200",
-          }}
-        >
-          Coffee
-        </Text>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
       </View>
     </View>
   );
@@ -27,7 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.colorWhite,
     justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
   },
 });
