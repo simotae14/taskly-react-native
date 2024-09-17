017839c39956e61d431f3ed491173d5694c7d56a
Simone Taeggi <startae14@gmail.com>
2024-09-17 11:49:29 +0200

    create a Delete button using TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Pressable,
-} from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => console.log("Pressed")}>
-        <Text>Delete</Text>
-      </Pressable>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          onPress={() => console.log("Pressed")}
+          activeOpacity={0.8}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -31,9 +29,23 @@ const styles = StyleSheet.create({
     borderBottomColor: theme.colorCerulean,
     paddingHorizontal: 8,
     paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   itemText: {
     fontSize: 18,
     fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
