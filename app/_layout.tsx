f6994d03f781df03dd1f34671a3f6775400aae4d
Simone Taeggi <startae14@gmail.com>
2024-09-19 15:55:50 +0200

    Change bottom Tab navigation icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,43 @@
 import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Shopping list" }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colorCerulean,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="counter"
         options={{
           title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
         }}
       />
-      <Tabs.Screen name="idea" options={{ title: "Idea" }} />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
