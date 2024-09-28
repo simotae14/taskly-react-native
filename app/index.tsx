28ca812f18f19c319c160e45e6996da6d0ef958c
Simone Taeggi <startae14@gmail.com>
2024-09-28 14:21:55 +0200

    Add a Text input and submit handler

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,53 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TextInput } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
+
+// define shopping list items type
+type ShoopingListItemType = {
+  id: string;
+  name: string;
+};
+
+// define the initialList
+const initialList: ShoopingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState(initialList);
+  const [value, setValue] = useState<string>();
+
+  const handleSubmit = () => {
+    if (value) {
+      // add new item to the list
+      const newShoppingList = [
+        {
+          id: new Date().toISOString(),
+          name: value,
+        },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Sugar" isCompleted />
+      <TextInput
+        value={value}
+        style={styles.textInput}
+        onChangeText={setValue}
+        placeholder="E.g Coffee"
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
     </View>
   );
 }
@@ -17,5 +57,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    backgroundColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    marginHorizontal: 12,
+    marginBottom: 12,
   },
 });
