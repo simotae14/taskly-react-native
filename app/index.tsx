dd01c0179611f738e2ef35bafaa11995169cf0d8
Simone Taeggi <startae14@gmail.com>
2024-10-01 01:12:03 +0200

    Data persistance shoppingList

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
 import { StyleSheet, TextInput, FlatList, View, Text } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getFromStorage, saveToStorage } from "../utils/storage";
+
+const storageKey = "shopping-list";
 
 // define shopping list items type
 type ShoppingListItemType = {
@@ -15,6 +18,16 @@ export default function App() {
   const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
   const [value, setValue] = useState<string>();
 
+  useEffect(() => {
+    const fetchInitial = async () => {
+      const data = await getFromStorage(storageKey);
+      if (data) {
+        setShoppingList(data);
+      }
+    };
+    fetchInitial();
+  }, []);
+
   const handleSubmit = () => {
     if (value) {
       // add new item to the list
@@ -27,7 +40,8 @@ export default function App() {
         ...shoppingList,
       ];
       setShoppingList(newShoppingList);
-      setValue("");
+      saveToStorage(storageKey, newShoppingList);
+      setValue(undefined);
     }
   };
 
@@ -49,6 +63,7 @@ export default function App() {
       }
       return item;
     });
+    saveToStorage(storageKey, newShoppingList);
     setShoppingList(newShoppingList);
   };
 
